79e5128f3571a5ff7e423679a68d32ad2f8c1ec6
unknown <ds03023@gmail.com>
2019-03-12 13:02:31 +0900

    upload baekjoon problem solution

---
+++ 자료구조/ConsoleApplication1/ConsoleApplication1/소스.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	int N, arr[1000];
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+	sort(arr, arr+N);
+	cout << arr[N - 2] + arr[N - 1];
+
+	return 0;
+}
