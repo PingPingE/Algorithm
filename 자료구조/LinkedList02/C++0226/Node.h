70a3bf3529cbbed8b14ff87789d82658517a4fee
unknown <ds03023@gmail.com>
2020-08-05 17:36:00 +0900

    empty this directory for re-uploading

--- 자료구조/LinkedList02/C++0226/Node.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-typedef struct node
-{
-	int data;
-	struct node* link;
-}Node;
--- 자료구조/Stack,Queue/Stack,Queue/error.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-static inline void error(char* msg) //error 처리
-{
-	std::cout << msg << std::endl;//해당 메세지를 출력하고
-	exit(1); //종료
-}
