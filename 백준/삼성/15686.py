06af95ba48ddac153cc5f983d951cc6d9765841a
unknown <ds03023@gmail.com>
2020-09-01 16:39:47 +0900

    solve a simulation problem

--- 백준/삼성/15686.py
+++ 백준/삼성/15686.py
@@ -1,32 +1,96 @@
-from itertools import combinations
-import sys
-import copy
-def sol(s):
-    global chicken, home, res
-    tmp = copy.copy(chicken)
-    tmp -= set(s)
-    total = 0
-    for y1,x1 in home:
-        dist=sys.maxsize
-        for y2,x2 in tmp:
-            dist=min(dist, abs(y1-y2)+abs(x1-x2))
-        total += dist
-    res = min(res, total)
+'''
+문제)
+크기가 N×N인 도시가 있다. 도시는 1×1크기의 칸으로 나누어져 있다. 도시의 각 칸은 빈 칸, 치킨집, 집 중 하나이다. 도시의 칸은 (r, c)와 같은 형태로 나타내고,
+r행 c열 또는 위에서부터 r번째 칸, 왼쪽에서부터 c번째 칸을 의미한다. r과 c는 1부터 시작한다.
+이 도시에 사는 사람들은 치킨을 매우 좋아한다. 따라서, 사람들은 "치킨 거리"라는 말을 주로 사용한다.
+치킨 거리는 집과 가장 가까운 치킨집 사이의 거리이다. 즉, 치킨 거리는 집을 기준으로 정해지며,
+각각의 집은 치킨 거리를 가지고 있다. 도시의 치킨 거리는 모든 집의 치킨 거리의 합이다.
+임의의 두 칸 (r1, c1)과 (r2, c2) 사이의 거리는 |r1-r2| + |c1-c2|로 구한다.
 
-N,M = map(int,input().split())
-chicken = set()
-home = set()
-res = sys.maxsize
-for _ in range(N):
+예를 들어, 아래와 같은 지도를 갖는 도시를 살펴보자.
+0 2 0 1 0
+1 0 1 0 0
+0 0 0 0 0
+0 0 0 1 1
+0 0 0 1 2
+
+0은 빈 칸, 1은 집, 2는 치킨집이다.
+(2, 1)에 있는 집과 (1, 2)에 있는 치킨집과의 거리는 |2-1| + |1-2| = 2, (5, 5)에 있는 치킨집과의 거리는 |2-5| + |1-5| = 7이다.
+따라서, (2, 1)에 있는 집의 치킨 거리는 2이다.
+(5, 4)에 있는 집과 (1, 2)에 있는 치킨집과의 거리는 |5-1| + |4-2| = 6, (5, 5)에 있는 치킨집과의 거리는 |5-5| + |4-5| = 1이다. \
+따라서, (5, 4)에 있는 집의 치킨 거리는 1이다.
+이 도시에 있는 치킨집은 모두 같은 프랜차이즈이다. 프렌차이즈 본사에서는 수익을 증가시키기 위해 일부 치킨집을 폐업시키려고 한다.
+오랜 연구 끝에 이 도시에서 가장 수익을 많이 낼 수 있는  치킨집의 개수는 최대 M개라는 사실을 알아내었다.
+도시에 있는 치킨집 중에서 최대 M개를 고르고, 나머지 치킨집은 모두 폐업시켜야 한다.
+어떻게 고르면, 도시의 치킨 거리가 가장 작게 될지 구하는 프로그램을 작성하시오.
+
+입력)
+첫째 줄에 N(2 ≤ N ≤ 50)과 M(1 ≤ M ≤ 13)이 주어진다.
+둘째 줄부터 N개의 줄에는 도시의 정보가 주어진다.
+도시의 정보는 0, 1, 2로 이루어져 있고, 0은 빈 칸, 1은 집, 2는 치킨집을 의미한다. 집의 개수는 2N개를 넘지 않으며, 적어도 1개는 존재한다.
+치킨집의 개수는 M보다 크거나 같고, 13보다 작거나 같다.
+
+출력)
+첫째 줄에 폐업시키지 않을 치킨집을 최대 M개를 골랐을 때, 도시의 치킨 거리의 최솟값을 출력한다.
+'''
+
+#sol1) 비트마스킹으로 조합
+#122960kb	292ms
+import sys
+def countBits(x):
+    cnt = 0
+    while x:
+        if x&1:
+            cnt += 1
+        x >>=1
+    return cnt
+N,M = map(int, input().split())
+#0:빈칸, 1:집, 2: 치킨집
+chicken = []
+home = []
+for n in range(N):
     li = list(map(int, sys.stdin.readline().split()))
-    for l in range(len(li)):
-        if li[l] == 2:
-            chicken.add((_,l))
-        elif li[l] == 1:
-            home.add((_,l))
-# 없애야할 치킨집 개수
-goal= len(chicken) - M
-#없앨 가게 조합
-for s in set(combinations(chicken, goal)):
-    sol(s)
+    for e,l in enumerate(li):
+        if l == 2:
+            chicken.append((n,e))
+        elif l==1:
+            home.append((n,e))
+res = sys.maxsize
+for i in range(1<<len(chicken)):
+    if countBits(i) == M:
+        target = []
+        for j in range(len(chicken)):
+            if i&1<<j:
+                target.append(chicken[j])
+        home_dist = {h:sys.maxsize for h in home}
+        for r,c in target:
+            for k,v in home_dist.items():
+                home_dist[k] = min(v,abs(k[0]-r)+abs(k[1]-c))
+        res = min(res, sum(home_dist.values()))
 print(res)
+
+#sol2) itertools 모듈 활용
+'''
+#122908kb	264ms
+import sys
+from itertools import combinations
+N,M = map(int, input().split())
+#0:빈칸, 1:집, 2: 치킨집
+chicken = []
+home = []
+for n in range(N):
+    li = list(map(int, sys.stdin.readline().split()))
+    for e,l in enumerate(li):
+        if l == 2:
+            chicken.append((n,e))
+        elif l==1:
+            home.append((n,e))
+res = sys.maxsize
+for target in combinations(chicken, M):
+    home_dist = {h:sys.maxsize for h in home}
+    for r,c in target:
+        for k,v in home_dist.items():
+            home_dist[k] = min(v,abs(k[0]-r)+abs(k[1]-c))
+    res = min(res, sum(home_dist.values()))
+print(res)
+'''
