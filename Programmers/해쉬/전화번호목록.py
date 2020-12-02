ca558ac146d16484e328298c4cad8cd3b060a299
unknown <ds03023@gmail.com>
2020-12-02 22:39:32 +0900

    algorithm studay 201202 hash

---
+++ Programmers/해쉬/전화번호목록.py
@@ -0,0 +1,41 @@
+'''
+문제 설명)
+전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 경우가 있는지 확인하려 합니다.
+전화번호가 다음과 같을 경우, 구조대 전화번호는 영석이의 전화번호의 접두사입니다.
+
+구조대 : 119
+박준영 : 97 674 223
+지영석 : 11 9552 4421
+전화번호부에 적힌 전화번호를 담은 배열 phone_book 이 solution 함수의 매개변수로 주어질 때, 
+어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요.
+
+제한 사항)
+phone_book의 길이는 1 이상 1,000,000 이하입니다.
+각 전화번호의 길이는 1 이상 20 이하입니다.
+'''
+
+def solution(phone_book):
+    for i in range(len(phone_book)):
+        for j in range(i+1,len(phone_book)):
+            if phone_book[i].startswith(phone_book[j]) or phone_book[j].startswith(phone_book[i]):
+                return False
+    return True
+
+
+'''
+정확성  테스트
+테스트 1 〉	통과 (0.01ms, 10.3MB)
+테스트 2 〉	통과 (0.01ms, 10.2MB)
+테스트 3 〉	통과 (0.01ms, 10.2MB)
+테스트 4 〉	통과 (0.01ms, 10.1MB)
+테스트 5 〉	통과 (0.01ms, 10.2MB)
+테스트 6 〉	통과 (0.01ms, 10.1MB)
+테스트 7 〉	통과 (0.01ms, 10.2MB)
+테스트 8 〉	통과 (0.00ms, 10.1MB)
+테스트 9 〉	통과 (0.00ms, 10.2MB)
+테스트 10 〉	통과 (0.01ms, 10.3MB)
+테스트 11 〉	통과 (0.01ms, 10.2MB)
+효율성  테스트
+테스트 1 〉	통과 (0.02ms, 10.9MB)
+테스트 2 〉	통과 (0.02ms, 10.8MB)
+'''
