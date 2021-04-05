7ddca624e3c56d49cec49fc15bd5024f8e92793f
unknown <ds03023@gmail.com>
2021-04-06 08:13:41 +0900

    add solution2 with bit masking

--- 백준/14889.py
+++ 백준/14889.py
@@ -1,10 +1,10 @@
 '''
 문제)
-오늘은 스타트링크에 다니는 사람들이 모여서 축구를 해보려고 한다. 축구는 평일 오후에 하고 의무 참석도 아니다. 
+오늘은 스타트링크에 다니는 사람들이 모여서 축구를 해보려고 한다. 축구는 평일 오후에 하고 의무 참석도 아니다.
 축구를 하기 위해 모인 사람은 총 N명이고 신기하게도 N은 짝수이다. 이제 N/2명으로 이루어진 스타트 팀과 링크 팀으로 사람들을 나눠야 한다.
 
-BOJ를 운영하는 회사 답게 사람에게 번호를 1부터 N까지로 배정했고, 아래와 같은 능력치를 조사했다. 
-능력치 Sij는 i번 사람과 j번 사람이 같은 팀에 속했을 때, 팀에 더해지는 능력치이다. 팀의 능력치는 팀에 속한 모든 쌍의 능력치 Sij의 합이다. 
+BOJ를 운영하는 회사 답게 사람에게 번호를 1부터 N까지로 배정했고, 아래와 같은 능력치를 조사했다.
+능력치 Sij는 i번 사람과 j번 사람이 같은 팀에 속했을 때, 팀에 더해지는 능력치이다. 팀의 능력치는 팀에 속한 모든 쌍의 능력치 Sij의 합이다.
 Sij는 Sji와 다를 수도 있으며, i번 사람과 j번 사람이 같은 팀에 속했을 때, 팀에 더해지는 능력치는 Sij와 Sji이다.
 
 N=4이고, S가 아래와 같은 경우를 살펴보자.
@@ -32,6 +32,7 @@ i번 줄의 j번째 수는 Sij 이다. Sii는 항상 0이고, 나머지 Sij는 1
 출력)
 첫째 줄에 스타트 팀과 링크 팀의 능력치의 차이의 최솟값을 출력한다.
 '''
+#sol1: 내장 함수로 조합 구하기
 #123512kb	884ms
 from itertools import combinations
 import sys
@@ -53,4 +54,36 @@ for comb in combinations(range(N),N//2):#=======조합 구하기
     team1=set(comb)
     team2=total-team1
     ans=min(ans, get_diff(team1,team2))
+print(ans)
+
+#sol2: bit masking으로 조합 구하기
+#123628kb	868ms
+import sys
+def get_team1(x):#=====bit 체크하면서 team1 set에 구성원 index 담기
+    tmp=set()
+    n=0
+    while x:
+        if x&1:tmp.add(n)
+        x>>=1
+        n+=1
+    return tmp
+
+def get_diff(t1,t2):
+    return abs(get_sum(t1)-get_sum(t2))
+
+def get_sum(t):
+    sum=0
+    for i in t:
+        for j in t:
+            sum+=S[i][j]
+    return sum
+
+N=int(input())
+S=[list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+ans=sys.maxsize
+total=set(range(N))
+for i in range(1<<N):
+    team1=get_team1(i)
+    if len(team1)==N//2:
+        ans=min(ans, get_diff(team1,total-team1))
 print(ans)
