93e0e5644d581d513b0b1f8f97af264f49602278
unknown <ds03023@gmail.com>
2021-04-25 23:56:45 +0900

    trying to solve a problem

--- 백준/6549.py
+++ 백준/6549.py
@@ -27,7 +27,7 @@ while True:
     #====복사 과정을 생략하기 위해 두 개의 덱큐를 선언
     que=[deque(),deque()]
     flag=0 #=== 하나의 큐는 현재 대상(que[flag]) / 나머지 하나는 갱신 값을 넣을 큐 (que[not flag])
-    
+
     for cur_i,cur_h in enumerate(h):
         if not que[flag]:
             que[flag].append((cur_i, cur_h))
@@ -50,9 +50,39 @@ while True:
 
         que[not flag].append((cur_i,cur_h))
         flag=not flag#===flag 갱신
-        
+
     # print(que[flag])
     while que[flag]:
         top_i,top_h=que[flag].pop()
         maxx = max(maxx, (n-top_i)*top_h)
     print(maxx)
+
+
+#https://hwiyong.tistory.com/379 코드
+while True:
+    n, *heights = list(map(int, input().split()))
+    if (n == 0):
+        break
+
+    # 첫 시점의 계산을 위해 0을 맨 앞에 추가하고,
+    heights.insert(0, 0)
+    # 마지막 사각형 계산을 위해 0을 끝에 추가합니다.
+    heights += [0]
+    checked = [0]
+    area = 0
+
+    # 현재 높이보다 이전 높이가 높으면, while에 진입합니다.
+    # 현재 높이가 더 낮은 경우, 넓이를 이어서 계산할 수 없으므로
+    # 이전 시점까지 저장됬던 사각형의 높이를 계산합니다.
+    # 끝 사각형도 고려해야 하므로, n+1까지 반복합니다.
+    for i in range(1, n + 2):
+        # heights[checked[-1]]은 이전 시점의 사각형 높이
+        # heights[i]는 현재 시점의 사각형 높이
+        # heights[checked[-1]] > heights[i]는 현재 높이보다 이전 높이가 높은지 확인
+        while (checked and (heights[checked[-1]] > heights[i])):
+            # 비교할 사각형 index
+            cur_height = checked.pop()
+            # (i - 1 - checked[-1])은 cur_height와 현재 시점 사이에 몇 개의 사각형이 존재하는지를 판단
+            area = max(area, (i - 1 - checked[-1]) * heights[cur_height])
+        checked.append(i)
+    print(area)
