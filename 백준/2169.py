20961f3f9c78f058e750b45a57bf299ec9da5bac
unknown <ds03023@gmail.com>
2021-05-12 22:50:03 +0900

    trying to solve a problem based on top-down DP

--- 백준/2169.py
+++ 백준/2169.py
@@ -1,28 +1,29 @@
-#===메모리 초과
-import sys, heapq
+#=======메모리 초과!!!!!!
+import sys
 sys.setrecursionlimit(10**8)
-dy,dx=[0,0,-1],[-1,1,0]
+INF=-987654321
+dy,dx=[0,0,1],[-1,1,0]
 N,M=map(int, input().split())
 board=[list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-costs=[[0]*M for _ in range(N)]
+costs=[[[INF]*3 for _ in range(M)] for _ in range(N)]
 done =[[0]*M for _ in range(N)]
+
 
 def check(y,x):
     return 0<=y<N and 0<=x<M
 
-def sol(cost,y,x):
+def sol(y,x,d):
     if (y,x) == (N-1,M-1):
-        return cost+board[y][x]
+        return board[y][x]
 
-    if not check(y,x) or done[y][x]:
-        return -1000
+    if costs[y][x][d] == INF:
+        done[y][x] = 1
+        for nd in range(3):
+            ny, nx = y + dy[nd], x + dx[nd]
+            if not check(ny, nx) or done[ny][nx]: continue
+            costs[y][x][d] = max(costs[y][x][d], sol(ny,nx,nd) + board[y][x])
+        done[y][x]=0
 
-    done[y][x]=1
-    costs[y][x]=max(sol(cost+board[y][x],y,x+1),
-                    sol(cost+board[y][x],y,x-1),
-                    sol(cost+board[y][x],y+1,x))
-    done[y][x]=0
-
-    return costs[y][x]
+    return costs[y][x][d]
 
 print(sol(0,0,0))
