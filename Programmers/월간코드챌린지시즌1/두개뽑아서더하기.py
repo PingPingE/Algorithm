9370991a4d98aef71f07f6222c1fa6b72685090d
unknown <ds03023@gmail.com>
2021-05-22 20:04:31 +0900

    solve basic math problems

---
+++ Programmers/월간코드챌린지시즌1/두개뽑아서더하기.py
@@ -0,0 +1,34 @@
+'''
+문제 설명
+정수 배열 numbers가 주어집니다. numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+
+제한사항
+numbers의 길이는 2 이상 100 이하입니다.
+numbers의 모든 수는 0 이상 100 이하입니다.
+
+입출력 예
+numbers	result
+[2,1,3,4,1]	[2,3,4,5,6,7]
+[5,0,2,7]	[2,5,7,9,12]
+
+'''
+
+from itertools import combinations
+def solution(numbers):
+    return sorted(set(sum(comb) for comb in combinations(numbers,2)))
+
+'''
+정확성  테스트
+테스트 1 〉	통과 (0.01ms, 10.3MB)
+테스트 2 〉	통과 (0.01ms, 10.2MB)
+테스트 3 〉	통과 (0.01ms, 10.2MB)
+테스트 4 〉	통과 (0.01ms, 10.2MB)
+테스트 5 〉	통과 (0.02ms, 10.2MB)
+테스트 6 〉	통과 (0.05ms, 10.2MB)
+테스트 7 〉	통과 (0.99ms, 10.2MB)
+테스트 8 〉	통과 (0.99ms, 10MB)
+테스트 9 〉	통과 (0.95ms, 10.2MB)
+채점 결과
+정확성: 100.0
+합계: 100.0 / 100.0
+'''
