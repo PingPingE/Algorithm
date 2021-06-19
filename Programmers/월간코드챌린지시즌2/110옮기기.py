2c8a442a2bf1731d2068be3f54118ddc183e3e30
unknown <ds03023@gmail.com>
2021-06-19 22:46:16 +0900

    trying to solve a problem

--- Programmers/월간코드챌린지시즌2/110옮기기.py
+++ Programmers/월간코드챌린지시즌2/110옮기기.py
@@ -11,18 +11,27 @@ def solution(s):
                 for _ in range(3):
                     que.popleft()
 
-        que = ''.join(que)
-        if len(que)<3:
-            idx=que.find('1')
-            if idx==-1:
-                answer.append(''.join(que)+'110'*cnt)
-            else:
-                answer.append(que[:idx] + '110'*cnt + que[idx:])
+        
+        '''
+        1. 0의 시작 위치를 찾는다. -> idx
+        2. 0이 없거나(idx==-1) 있어도 110 이상인 경우(idx>=2)는 '110'*cnt를 가장 앞에
+        3. 그 외 -> q_str[:idx+0개수] + '110'*cnt + q_str[idx+0개수:]
+        '''
+        #1
+        q_str = ''.join(que)
+        idx=q_str.find('0')
+        print(q_str, idx)
+        
+        #2
+        if idx==-1 or idx >=2:
+            answer.append('110'*cnt + q_str)
         else:
-            idx=que.find('111')
-            if idx==-1:
-                answer.append(''.join(que)+'110'*cnt )
-            else:
-                answer.append(que[:idx] + '110'*cnt + que[idx:] )
+            #3
+            z_cnt = 0
+            for i in range(idx, len(que)):
+                if que[i] =='0':
+                    z_cnt+=1
+                else: break
+            answer.append(q_str[:idx+z_cnt] + '110'*cnt + q_str[idx+z_cnt:] )
 
     return answer
