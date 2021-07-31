ff6611f3985fad6c224d930aaf4ee6d56443540d
unknown <ds03023@gmail.com>
2021-07-31 23:37:33 +0900

    trying to solve a problem

--- Programmers/2017팁스타운/사칙연산.py
+++ Programmers/2017팁스타운/사칙연산.py
@@ -1,19 +1,124 @@
 #시도 중
+#======version2(조금 개선)
 import sys
-sys.setrecursionlimit(10**8)
+sys.setrecursionlimit(10 ** 8)
+
+def cal(a, b, op):
+    if op == '+':
+        return a + b
+    return a - b
+
+
+def sol(nums, ops, flag, value):
+    if len(ops) == 1:
+        if flag:#왼쪽(최댓값)
+            value = max(value, cal(nums[0], nums[1], ops[0]))
+        else:#오른쪽(최솟값)
+            value = min(value, cal(nums[0], nums[1], ops[0]))
+        return value
+
+    if len(nums) == 1:
+        return nums[0]
+
+    for i in range(len(ops)):
+        tmp = cal(nums[i], nums[i + 1], ops[i])
+        v = sol(nums[:i] + [tmp] + nums[i + 2:], ops[:i] + ops[i + 1:], flag, value)
+        value = v
+
+    return value
+
+
 def solution(arr):
-    N=len(arr)
-    memo= [0 for _ in range(N)]
-    def sol(idx):
-        sum=0
-        for i in range(idx,N):
-            #'-' 나오면 쪼개기
-            if arr[i] =='-':
-                tmp = int(arr[i+1])
-                ret = sol(i+2)
-                return max(sum-tmp+ret, sum-(tmp+ret))
-            else:
-                if arr[i]=='+': continue
-                sum+=int(arr[i])
-        return sum
-    return sol(0)
+    INF = 987654321
+    ans = -INF
+    nums, ops = [int(arr[i]) for i in range(0, len(arr), 2)], [arr[i] for i in range(1, len(arr), 2)]
+
+    # 1. -만 골라낸다.
+    # 2. 그 - 기준으로 쪼갠다
+    # 3. 왼쪽은 max값 / 오른쪽은 min값을 도출해서 빼준다. -> ans 갱신
+
+    for i in range(len(ops)):
+        if ops[i] == '-':
+            left = sol(nums[:i + 1], ops[:i], 1, -INF)
+            right = sol(nums[i + 1:], ops[i + 1:], 0, INF)
+            ans = max(ans, left - right)
+
+    return sum(nums) if ans == -INF else ans
+
+
+'''
+정확성  테스트
+테스트 1 〉	통과 (551.20ms, 10.4MB)
+테스트 2 〉	통과 (80.79ms, 10.4MB)
+테스트 3 〉	통과 (0.03ms, 10.4MB)
+테스트 4 〉	통과 (0.03ms, 10.4MB)
+테스트 5 〉	통과 (1304.31ms, 10.4MB)
+테스트 6 〉	통과 (1302.82ms, 10.5MB)
+테스트 7 〉	통과 (139.46ms, 10.5MB)
+테스트 8 〉	통과 (645.93ms, 10.5MB)
+테스트 9 〉	통과 (0.10ms, 10.4MB)
+테스트 10 〉	통과 (0.02ms, 10.4MB)
+효율성  테스트
+테스트 1 〉	실패 (시간 초과)
+테스트 2 〉	실패 (시간 초과)
+테스트 3 〉	실패 (시간 초과)
+테스트 4 〉	실패 (시간 초과)
+테스트 5 〉	통과 (0.07ms, 10.5MB)
+테스트 6 〉	통과 (0.06ms, 10.5MB)
+테스트 7 〉	실패 (시간 초과)
+테스트 8 〉	실패 (시간 초과)
+'''
+
+# =====version 1
+import sys
+
+sys.setrecursionlimit(10 ** 8)
+ans = -987654321
+
+
+def cal(a, b, op):
+    if op == '+':
+        return a + b
+    return a - b
+
+
+def sol(nums, ops):
+    global ans
+    if len(ops) == 1:
+        ans = max(ans, cal(nums[0], nums[1], ops[0]))
+        return
+
+    for i in range(len(ops)):
+        tmp = cal(nums[i], nums[i + 1], ops[i])
+        sol(nums[:i] + [tmp] + nums[i + 2:], ops[:i] + ops[i + 1:])
+
+
+def solution(arr):
+    # 숫자와 연산자 분리
+    nums, ops = [int(arr[i]) for i in range(0, len(arr), 2)], [arr[i] for i in range(1, len(arr), 2)]
+    sol(nums, ops)
+    return ans
+
+
+'''
+정확성  테스트
+테스트 1 〉	통과 (5641.36ms, 10.4MB)
+테스트 2 〉	통과 (5656.25ms, 10.4MB)
+테스트 3 〉	통과 (5514.34ms, 10.4MB)
+테스트 4 〉	통과 (5534.46ms, 10.5MB)
+테스트 5 〉	통과 (5668.97ms, 10.4MB)
+테스트 6 〉	통과 (5643.12ms, 10.4MB)
+테스트 7 〉	통과 (5611.70ms, 10.5MB)
+테스트 8 〉	통과 (5755.72ms, 10.4MB)
+테스트 9 〉	통과 (0.35ms, 10.5MB)
+테스트 10 〉	통과 (0.02ms, 10.4MB)
+효율성  테스트
+테스트 1 〉	실패 (시간 초과)
+테스트 2 〉	실패 (시간 초과)
+테스트 3 〉	실패 (시간 초과)
+테스트 4 〉	실패 (시간 초과)
+테스트 5 〉	실패 (시간 초과)
+테스트 6 〉	실패 (시간 초과)
+테스트 7 〉	실패 (시간 초과)
+테스트 8 〉	실패 (시간 초과)
+'''
