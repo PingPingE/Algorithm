1f08db49f9c2be1ccee0907e3646a4fd781804e3
unknown <ds03023@gmail.com>
2021-08-25 23:58:30 +0900

    trying to solve a problem

--- 백준/11049.py
+++ 백준/11049.py
@@ -1,25 +1,36 @@
-#dp 문제인듯
 import sys
-sys.setrecursionlimit(10**9)
+INF = sys.maxsize
 N = int(input())
-mini = sys.maxsize
 arr = [list(map(int,input().split())) for _ in range(N)]
+memo= [[INF for j in range(N)] for i in range(N)]
 
-def dfs(num,cur, left, right):
-    global mini
-    if num >= mini: return
-    if not left and not right:
-        mini=min(mini, num)
-        return
+for i in range(N-1):
+    memo[i][i] =0
+    memo[i][i+1] = arr[i][0] * arr[i][1] * arr[i+1][1]
+memo[-1][-1] = 0
 
-    if left:
-        top = left[-1]
-        dfs(num+(top[0]*top[1]*cur[1]),(top[0],cur[1]), left[:-1], right[:])
+for k in range(N-1):
+    for i in range(k):
+        for j in range(k+1,N):
+            memo[i][j] = min(memo[i][k-1] + memo[k][j] + (arr[i][0] * arr[j][0] * arr[j][1]),memo[i][j])
+print(memo)
+print(memo[0][-1])
 
-    if right:
-        top = right[0]
-        dfs(num + (cur[0] * cur[1] * top[1]), (cur[0], top[1]), left[:], right[1:])
-
-for n in range(N):
-    dfs(0,arr[n],arr[:n],arr[n+1:])
-print(mini)
+# def dfs(num,cur, left, right):
+#     global mini
+#     if num >= mini: return
+#     if not left and not right:
+#         mini=min(mini, num)
+#         return
+#
+#     if left:
+#         top = left[-1]
+#         dfs(num+(top[0]*top[1]*cur[1]),(top[0],cur[1]), left[:-1], right[:])
+#
+#     if right:
+#         top = right[0]
+#         dfs(num + (cur[0] * cur[1] * top[1]), (cur[0], top[1]), left[:], right[1:])
+#
+# for n in range(N):
+#     dfs(0,arr[n],arr[:n],arr[n+1:])
+# print(mini)
