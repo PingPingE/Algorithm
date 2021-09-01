90a784f735dd42eb371650ecc3414b240472a49e
unknown <ds03023@gmail.com>
2021-09-02 06:02:54 +0900

    solve a problem

--- Programmers/카카오/카드짝맞추기.py
+++ Programmers/카카오/카드짝맞추기.py
@@ -1,4 +1,3 @@
-# 시도 중
 from collections import deque, defaultdict
 def solution(board, r, c):
     INF = 987654321
@@ -64,13 +63,45 @@ def solution(board, r, c):
         for k in remain:
             v1 = dic[k][0]
             v2 = dic[k][1]
-            que.append([v1, cnt + get_count(rc, v1, remain) + get_count(v1, v2, remain) + 2, remain - {k}])
-            que.append([v2, cnt + get_count(rc, v2, remain) + get_count(v2, v1, remain) + 2, remain - {k}])
+            #=====지금껏 틀린 이유: 도착점(다음 rc)을 잘못 넣음....  (rc -> v1 -> v2 순으로 가놓고 도착점을 v2가 아닌 v1으로 해놓음)
+            que.append([v2, cnt + get_count(rc, v1, remain) + get_count(v1, v2, remain) + 2, remain - {k}])
+            que.append([v1, cnt + get_count(rc, v2, remain) + get_count(v2, v1, remain) + 2, remain - {k}])
 
     return ans
-print(solution([[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]],1,0))
-# print(solution([[3, 0, 0, 2], [0, 0, 1, 0], [0, 1, 0, 0], [2, 0, 0, 3]], 0, 1))
-# print(solution(	[[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1]], 1, 1))
+
+'''
+정확성  테스트
+테스트 1 〉	통과 (3.31ms, 10.2MB)
+테스트 2 〉	통과 (2.93ms, 10.3MB)
+테스트 3 〉	통과 (3.30ms, 10.3MB)
+테스트 4 〉	통과 (2.71ms, 10.3MB)
+테스트 5 〉	통과 (23.00ms, 10.3MB)
+테스트 6 〉	통과 (21.81ms, 10.3MB)
+테스트 7 〉	통과 (27.75ms, 10.4MB)
+테스트 8 〉	통과 (25.48ms, 10.2MB)
+테스트 9 〉	통과 (248.70ms, 11.2MB)
+테스트 10 〉	통과 (259.57ms, 11.1MB)
+테스트 11 〉	통과 (225.13ms, 11.2MB)
+테스트 12 〉	통과 (236.59ms, 11.2MB)
+테스트 13 〉	통과 (3040.68ms, 24.6MB)
+테스트 14 〉	통과 (3305.43ms, 24.6MB)
+테스트 15 〉	통과 (2619.73ms, 24.6MB)
+테스트 16 〉	통과 (3121.31ms, 24.5MB)
+테스트 17 〉	통과 (0.13ms, 10.2MB)
+테스트 18 〉	통과 (0.07ms, 10.2MB)
+테스트 19 〉	통과 (0.62ms, 10.2MB)
+테스트 20 〉	통과 (0.48ms, 10.3MB)
+테스트 21 〉	통과 (19.52ms, 10.3MB)
+테스트 22 〉	통과 (3462.06ms, 24.6MB)
+테스트 23 〉	통과 (3192.93ms, 24.5MB)
+테스트 24 〉	통과 (27.22ms, 10.2MB)
+테스트 25 〉	통과 (3406.72ms, 24.6MB)
+테스트 26 〉	통과 (25.30ms, 10.2MB)
+테스트 27 〉	통과 (27.75ms, 10.3MB)
+테스트 28 〉	통과 (3.76ms, 10.2MB)
+테스트 29 〉	통과 (3.10ms, 10.3MB)
+테스트 30 〉	통과 (3.10ms, 10.2MB)
+'''
 
 
 #삽질 기록=============
