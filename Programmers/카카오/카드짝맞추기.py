7bb3029f13b04576bd6229b61b040da6c6ecf0c2
unknown <ds03023@gmail.com>
2021-09-01 23:47:24 +0900

    trying to solve a problem

--- Programmers/카카오/카드짝맞추기.py
+++ Programmers/카카오/카드짝맞추기.py
@@ -12,20 +12,14 @@ def solution(board, r, c):
             if board[i][j]:
                 dic[board[i][j]].append((i, j))
 
-    len_dic = len(dic)
-
+    # rc1 -> rc2로 가는 최단거리
     def get_count(rc1, rc2, remain):
-        if rc1 == rc2:
-            return 0
-        print("====From:", rc1, "  TO:", rc2, " remain:", remain)
         m_que = deque([[rc1, 0]])
         done = set([rc1])
         cnt = INF
         while m_que:
             rc, tmp_cnt = m_que.popleft()
-            print("처음:", rc)
             if rc == rc2:
-                print("도착:",tmp_cnt)
                 cnt = min(tmp_cnt, cnt)
                 continue
 
@@ -34,14 +28,13 @@ def solution(board, r, c):
 
             for d in range(4):
                 ny, nx = rc[0] + dy[d], rc[1] + dx[d]
-                if ny<0 or nx<0 or ny>3 or nx>3: continue
-                if (ny,nx) not in done:
+                if ny < 0 or nx < 0 or ny > 3 or nx > 3: continue
+                # 그냥 해당 방향으로 한칸 이동
+                if (ny, nx) not in done:
                     done.add((ny, nx))
-                    # 그냥 이동
-                    print("그냥 이동:" , (ny,nx))
                     m_que.append([(ny, nx), tmp_cnt + 1])
 
-                # 컨트롤
+                # 컨트롤 + 방향키 이동(카드가 처음 나오는 칸까지 or 마지막 칸까지)
                 cy, cx = ny, nx
                 while board[cy][cx] not in remain:
                     if 0 <= cy + dy[d] < 4 and 0 <= cx + dx[d] < 4:
@@ -49,16 +42,17 @@ def solution(board, r, c):
                         cx += dx[d]
                     else:
                         break
-                if (ny, nx) == (cy, cx):
+
+                if (cy, cx) in done:
                     continue
                 done.add((cy, cx))
-                print("컨트롤 이동:", (cy, cx))
                 m_que.append([(cy, cx), tmp_cnt + 1])
         return cnt
 
     que.append([(r, c), 0, set(dic.keys())])
     while que:
         rc, cnt, remain = que.popleft()
+        # print(rc, cnt, remain)
         if not remain:
             ans = min(ans, cnt)
             continue
@@ -66,14 +60,14 @@ def solution(board, r, c):
         if cnt >= ans:
             continue
 
+        # 어느 카드부터 짝 맞출지
         for k in remain:
             v1 = dic[k][0]
             v2 = dic[k][1]
-            que.append([v1, cnt + get_count(rc, v1, remain) + get_count(v1, v2, remain), remain - {k}])
-            que.append([v2, cnt + get_count(rc, v2, remain) + get_count(v2, v1, remain), remain - {k}])
+            que.append([v1, cnt + get_count(rc, v1, remain) + get_count(v1, v2, remain) + 2, remain - {k}])
+            que.append([v2, cnt + get_count(rc, v2, remain) + get_count(v2, v1, remain) + 2, remain - {k}])
 
-    return ans + len_dic * 2
-
+    return ans
 print(solution([[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]],1,0))
 # print(solution([[3, 0, 0, 2], [0, 0, 1, 0], [0, 1, 0, 0], [2, 0, 0, 3]], 0, 1))
 # print(solution(	[[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1]], 1, 1))
