faaf9d9a406e31743ab396961364fd328ec0b9a1
unknown <ds03023@gmail.com>
2021-09-04 23:57:43 +0900

    trying to solve a problem

--- Programmers/카카오/매출하락최소화.py
+++ Programmers/카카오/매출하락최소화.py
@@ -1,56 +1,42 @@
-#시간 초과 ㅠ
+#시도 중
+'''
+1. 각 팀(팀장 기준)에서 팀원 중 가장 매출이 적은 팀원의 매출 저장(mini)
+2. res[cur] = res[prev] + min(sales[prev-1], mini[cur]) #이전 레벨의 팀장인 팀원이 참가 or 가장 매출이 적은 팀원이 참가
+'''
 from collections import defaultdict
-import sys
-
-sys.setrecursionlimit(10 ** 8)
-
 def solution(sales, links):
-    teams = defaultdict(set)
-    link = {}
-    link[1] = 0  # value: 상위 노드
-    for p, c in links:
-        teams[p].add(c)
-        link[c] = p
-
+    answer = 0
+    INF=987654321
+    mini = defaultdict(lambda:INF) #key: 팀장, value: 해당 팀의 팀원 중 가장 최소 매출액
+    link = defaultdict(int) #key:노드, value: 부모 노드
+    link[1]=0
+    for t1,t2 in links:
+        mini[t1] = min(sales[t1-1], min(sales[t2-1], mini[t1]))
+        link[t2] = t1
+    print(mini)
     def get_degree(x, cnt):
         if x == 1:
             return cnt
         return get_degree(link[x], cnt + 1)
 
-    degree = {}
-    for k in teams:
-        degree[k] = get_degree(k, 0)
+    degree = defaultdict(set) #key: degree, value: 해당하는 노드set
 
-    answer = 987654321
-    leaders = set(teams.keys())
+    for k in link:
+        degree[get_degree(k, 0)].add(k)
 
-    print(degree)
+    res=defaultdict(lambda:INF)
+    # print(link)
 
-    def dfs(teams, done, ans):
-        nonlocal answer
-        print(done, ans)
-        if len(done) == len(teams):
-            answer = min(answer, ans)
+    def dfs(prev, cur):#이전 팀장 번호, 현 팀장 번호
+        nonlocal res
+        if cur == 0:
             return
-
-        for k, v in sorted(teams.items(), key=lambda x: degree[x[0]]):
-            if k in done:
-                continue
-            print("leader:", k)
-            # 팀장 중 최소
-            tmp_set = (leaders & v)
-            if tmp_set:
-                t_x = min(tmp_set, key=lambda x: sales[x - 1])
-                dfs(teams, done | {k, t_x}, ans + sales[t_x - 1])
-
-            # 팀원 중 최소
-            tmp_set2 = v - leaders
-            if tmp_set2:
-                m_x = min(tmp_set2 | {k}, key=lambda x: sales[x - 1])
-                dfs(teams, done | {k}, ans + sales[m_x - 1])
-
-    dfs(teams, set(), 0)
-    return answer
+        res[cur] = res[prev] + min(sales[prev-1], mini[cur])
+        dfs(cur, link[cur])
 
 
-print(solution([14, 17, 15, 18, 19, 14, 13, 16, 28, 17]	,[[10, 8], [1, 9], [9, 7], [5, 4], [1, 5], [5, 10], [10, 6], [1, 3], [10, 2]]))
+    for i in degree[max(degree)]:
+        res[i]=0
+        dfs(i, link[i])
+    print(res)
+    return res[1]
