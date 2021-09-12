886b9523c4c87d634c0abb7b54217039f129f711
PingPingE <ds03023@gmail.com>
2021-09-12 21:40:02 +0900

    trying to solve a problem

--- Programmers/카카오/매칭점수.py
+++ Programmers/카카오/매칭점수.py
@@ -1,66 +1,99 @@
-#시도 중!
+#시도 중
 import re
 from collections import defaultdict
+
 def solution(word, pages):
     answer = 0
     word = word.lower()
-    #url을 index에 매핑
-    page_dic = {}#key: url, value: index
+    # url을 index에 매핑
+    page_dic = {}  # key: url, value: index
 
-    #자기 지신에게 링크를 건 링크 idx set
-    link_dic = defaultdict(set)#key: url, value: 링크(idx) set
-    extern_score = [0 for _ in range(len(pages))]
+    # 링크 점수
+    link_score = [0 for _ in range(len(pages))]
+
+    # 자기 자신(key)이 외부로 링크를 건 링크 url set(value)
+    extern_dic = defaultdict(set)  # key: idx, value: url set
     basic_score = [0 for _ in range(len(pages))]
 
     for idx, page in enumerate(pages):
         page = page.lower()
 
-        tmp = page.split('</head>')
-        target = tmp[0].split('content=')[-1].split('/>')[0]
+        # 태그로 구분하지말고 일단 url만 추출하기
+        links = re.findall('''\"https://.*\"''', page)
+
+        # 맨 첫 url은 대상 링크(target) 나머진 외부 링크
+        target = links[0]
         page_dic[target] = idx
 
-        a_tag= re.findall('<a href=.*</a>', tmp[-1])
+        # 외부 링크 set
+        extern_dic[idx].update(list(links[i] for i in range(1, len(links))))
 
-        #외부 링크 수
-        extern_score[idx] = len(a_tag)
+        # body부분만 자르고 a태그 기준 나누기
+        body_tag = page.split('<body>')[1].split('</body>')[0].split('<a')
+        body_candi = []
+        if len(body_tag) == 1:
+            body_candi = body_tag[0]
+        else:
+            for body in body_tag:
+                tmp = body.split('</a>')
+                body_candi.append(tmp[-1])
 
-        body_tag= tmp[-1].split('</body>')[0].split('<body>')[-1].strip()
-        for a in a_tag:
-            try:#========body_tag에서 a 태그 제거하는 이 부분이 문제같은데(런타임에러)
-                # print("before: ", body_tag, ' a: ', a)
-                body_tag = re.sub(a, '\n',body_tag).strip()
-                # print("after: ", body_tag)
-                link = a.split('href=')[-1].split('>')[0]
-                #link를 링크로 건 현재 url의 idx를 set에 저장
-                link_dic[link].add(idx)
-            except: continue
+        body_candi = ''.join(body_candi)
+        # 문자만 남기기
+        body_candi = re.sub('[^a-z]', ' ', body_candi).strip()
 
-        body_tag = re.sub('[^a-z]',' ', body_tag).strip()
-        # print("========final: ",body_tag)
+        # 기본 점수
+        for w in body_candi.split():
+            if w == word:
+                basic_score[idx] += 1
 
-        #기본 점수
-        for w in body_tag.split():
-            print(w)
-            if w==word:
-                basic_score[idx]+=1
+    ans = [0, 100]  # 점수, index
 
-    ans = [0,100] #점수, index
-
-    #링크 점수, 매칭 점수 구하기
-    for k,v in page_dic.items():
-        link_score=0
-        #링크 점수 계산
-        for idx in link_dic[k]:
+    # 링크 점수, 매칭 점수 구하기
+    for k, v in page_dic.items():
+        link_score = 0
+        # 링크 점수 계산
+        for k2, v2 in extern_dic.items():
             try:
-                link_score += basic_score[idx] / extern_score[idx]
+                if k in v2:
+                    link_score += basic_score[k2] / len(extern_dic[k2])
             except:
                 continue
-        #매치 점수 계산
-        match_score= basic_score[v] + link_score
+
+        # 매치 점수 계산
+        match_score = basic_score[v] + link_score
         if match_score > ans[0]:
             ans[0], ans[1] = match_score, v
 
-        elif match_score==ans[0]:
+        elif match_score == ans[0]:
             ans[1] = min(ans[1], v)
 
     return ans[1]
+
+'''
+정확성  테스트
+테스트 1 〉	통과 (0.64ms, 10.4MB)
+테스트 2 〉	통과 (0.53ms, 10.3MB)
+테스트 3 〉	통과 (0.49ms, 10.3MB)
+테스트 4 〉	실패 (0.55ms, 10.2MB)
+테스트 5 〉	통과 (0.61ms, 10.3MB)
+테스트 6 〉	실패 (0.61ms, 10.4MB)
+테스트 7 〉	통과 (0.58ms, 10.3MB)
+테스트 8 〉	실패 (0.59ms, 10.2MB)
+테스트 9 〉	실패 (0.59ms, 10.3MB)
+테스트 10 〉	실패 (0.53ms, 10.3MB)
+테스트 11 〉	통과 (0.45ms, 10.2MB)
+테스트 12 〉	통과 (0.42ms, 10.3MB)
+테스트 13 〉	통과 (0.42ms, 10.3MB)
+테스트 14 〉	통과 (0.49ms, 10.3MB)
+테스트 15 〉	통과 (0.43ms, 10.3MB)
+테스트 16 〉	통과 (0.21ms, 10.2MB)
+테스트 17 〉	실패 (0.37ms, 10.3MB)
+테스트 18 〉	통과 (0.17ms, 10.3MB)
+테스트 19 〉	통과 (0.23ms, 10.3MB)
+테스트 20 〉	통과 (0.48ms, 10.3MB)
+
+채점 결과
+정확성: 70.0
+합계: 70.0 / 100.0
+'''
