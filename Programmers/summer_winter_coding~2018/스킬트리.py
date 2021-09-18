104597f79501586adcb5e486bb5828a40a21840c
PingPingE <ds03023@gmail.com>
2021-09-19 00:08:29 +0900

    solve a problem

--- Programmers/summer_winter_coding~2018/스킬트리.py
+++ Programmers/summer_winter_coding~2018/스킬트리.py
@@ -1,21 +1,62 @@
-# 0. skill에서 모든 가능한 문자열 남기기(candi)
-# 1. skill에 있는 문자만 남기기
-# 2. candi에 1.이후 남은 문자열이 있는지 확인
-import re
+'''
+문제 설명)
+선행 스킬이란 어떤 스킬을 배우기 전에 먼저 배워야 하는 스킬을 뜻합니다.
 
+예를 들어 선행 스킬 순서가 스파크 → 라이트닝 볼트 → 썬더일때, 썬더를 배우려면 먼저 라이트닝 볼트를 배워야 하고, 라이트닝 볼트를 배우려면 먼저 스파크를 배워야 합니다.
+
+위 순서에 없는 다른 스킬(힐링 등)은 순서에 상관없이 배울 수 있습니다. 따라서 스파크 → 힐링 → 라이트닝 볼트 → 썬더와 같은 스킬트리는 가능하지만,
+썬더 → 스파크나 라이트닝 볼트 → 스파크 → 힐링 → 썬더와 같은 스킬트리는 불가능합니다.
+
+선행 스킬 순서 skill과 유저들이 만든 스킬트리1를 담은 배열 skill_trees가 매개변수로 주어질 때, 가능한 스킬트리 개수를 return 하는 solution 함수를 작성해주세요.
+
+제한 조건)
+스킬은 알파벳 대문자로 표기하며, 모든 문자열은 알파벳 대문자로만 이루어져 있습니다.
+스킬 순서와 스킬트리는 문자열로 표기합니다.
+예를 들어, C → B → D 라면 "CBD"로 표기합니다
+선행 스킬 순서 skill의 길이는 1 이상 26 이하이며, 스킬은 중복해 주어지지 않습니다.
+skill_trees는 길이 1 이상 20 이하인 배열입니다.
+skill_trees의 원소는 스킬을 나타내는 문자열입니다.
+skill_trees의 원소는 길이가 2 이상 26 이하인 문자열이며, 스킬이 중복해 주어지지 않습니다.
+
+'''
 def solution(skill, skill_trees):
-    candi = set([sk for sk in skill])
+    #0은 순서 상관 X , 나머지는(1이상) 낮은 원소값을 가지는게 먼저 나와야함
+    order = [0 for _ in range(26)]
     ans = 0
 
-    for n in range(1, len(skill) + 1):
-        candi.add(skill[:n])
+    #순서 입력
+    for i in range(len(skill)):
+        order[ord(skill[i]) - 65] = i+1
 
-    candi.add('')
+    for sk in skill_trees:
+        prev = 0
 
-    for sk in skill_trees: a
-    filtered_sk = re.sub(f"[^{skill}]", '', sk)
-    if filtered_sk in candi:
-        # print(filtered_sk, sk)
-        ans += 1
+        for s in sk:
+            target = ord(s) - 65
+            if order[target]==0:continue
+            if order[target] == prev+1:
+                prev = order[target]
+            else:
+                break
+        else:
+            ans += 1
 
-return ans
+    return ans
+
+'''
+정확성  테스트
+테스트 1 〉	통과 (0.01ms, 10.2MB)
+테스트 2 〉	통과 (0.01ms, 10.2MB)
+테스트 3 〉	통과 (0.01ms, 10.2MB)
+테스트 4 〉	통과 (0.01ms, 10.2MB)
+테스트 5 〉	통과 (0.01ms, 10.2MB)
+테스트 6 〉	통과 (0.01ms, 10.2MB)
+테스트 7 〉	통과 (0.01ms, 10.3MB)
+테스트 8 〉	통과 (0.01ms, 10.3MB)
+테스트 9 〉	통과 (0.01ms, 10.3MB)
+테스트 10 〉	통과 (0.01ms, 10.2MB)
+테스트 11 〉	통과 (0.01ms, 10.2MB)
+테스트 12 〉	통과 (0.01ms, 10.2MB)
+테스트 13 〉	통과 (0.01ms, 10.2MB)
+테스트 14 〉	통과 (0.01ms, 10.2MB)
+'''
