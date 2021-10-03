37aa2e1e35285fea7d2878ac78221aa44359736f
PingPingE <ds03023@gmail.com>
2021-10-03 23:41:14 +0900

    trying to solve a problem

--- Programmers/summer_winter_coding~2018/스티커모으기(2).py
+++ Programmers/summer_winter_coding~2018/스티커모으기(2).py
@@ -1,34 +1,33 @@
 #시도중 
 import sys
-
-sys.setrecursionlimit(10 ** 8)
+sys.setrecursionlimit(10**8)
 def solution(sticker):
     answer = 0
     copy = sticker[:]
     N = len(sticker)
 
-    def dfs(sum, cnt):
+    def dfs(ans, sum_):
         nonlocal answer, copy
-        if cnt == 0:
-            answer = max(sum, answer)
+        if sum_ == 0:
+            answer = max(ans, answer)
             return
 
         for i in range(N):
             if copy[i] != -1:
                 right = i + 1 if i + 1 < N else 0
                 left = i - 1
-                flag = [0, 0]
+                flag = [0,0]
 
                 copy[i] = -1
                 if copy[left] != -1:
                     copy[left] = -1
-                    flag[0] = 1
-
+                    flag[0] =1
+                    
                 if copy[right] != -1:
                     copy[right] = -1
                     flag[1] = 1
 
-                dfs(sum + sticker[i], cnt - 1)
+                dfs(ans + sticker[i], sum_-(sticker[i]+flag[0]*sticker[left]+flag[1]*sticker[right]))
 
                 copy[i] = sticker[i]
                 if flag[0]:
@@ -36,6 +35,5 @@ def solution(sticker):
                 if flag[1]:
                     copy[right] = sticker[right]
 
-    dfs(0, N // 2)
+    dfs(0, sum(copy))
     return answer
-
