870ec7d63bc7a1f7d15f8c348041703737472015
PingPingE <ds03023@gmail.com>
2021-11-26 23:47:09 +0900

    trying to solve a problem

--- 백준/LCS4.py
+++ 백준/LCS4.py
@@ -2,18 +2,26 @@
 N = int(input())
 A, B= list(map(int, input().split())), list(map(int, input().split()))
 ans =0
+start, end = 0, N
 
+#처음과 마지막 원소가 같지 않을때까지 pop
+while  start<=end and A[start] == B[start]:
+    start+=1
+
+while start <= end and A[end-1] == B[end-1]:
+    end-=1
+
+N= len(A)
 #이전 행의 값만 참고하면 되지않나
 arr = [[0]*(N+1) for _ in range(2)]
 flag = 0
 
-for i in range(1,N+1):
-    for j in range(1,N+1):
+for i in range(start+1, end+1):
+    for j in range(start+1, end+1):
         if A[i-1] == B[j-1]:
             arr[flag][j] = arr[not flag][j-1]+1
             ans = max(ans, arr[flag][j])
         else:
             arr[flag][j] = max(arr[not flag][j], arr[flag][j-1])
     flag= not flag
-
-print(ans)
+print(ans+start+N-end)
