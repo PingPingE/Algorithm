5413508d470811ed0a7f8a5100a03ea55bbe3909
PingPingE <ds03023@gmail.com>
2022-05-11 00:26:39 +0900

    solve a problenm

--- 백준/연결요소의개수.py
+++ 백준/연결요소의개수.py
@@ -9,4 +9,32 @@
 출력
 첫째 줄에 연결 요소의 개수를 출력한다.
 '''
+#115700kb	360ms
+import sys
+def union(x,y):
+    global links
+    x_ = find(x)
+    y_ = find(y)
+    if x_ < y_:
+        links[y_]= x_
+    else:
+        links[x_] = y_
 
+def find(x):
+    global links
+    if links[x] == x:
+        return x
+    links[x] = find(links[x])
+    return links[x]
+
+N,M =map(int, input().split())
+links = {k:k for k in range(1,N+1) }
+for _ in range(M):
+    x,y = map(int, sys.stdin.readline().split())
+    union(x,y)
+
+#마지막에 하나씩 다 확인하고 parent 갱신해줘야함 (이것때매 계속 80%대에서 ㅂㄷㅂㄷ)
+for k in links:
+    find(k)
+
+print(len(set(links.values())))
