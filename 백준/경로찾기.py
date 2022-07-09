90918b5a5f505eb7e9f48635faa43feb56efd505
PingPingE <ds03023@gmail.com>
2022-07-09 14:06:27 +0900

    trying to solve a problem

--- 백준/경로찾기.py
+++ 백준/경로찾기.py
@@ -9,15 +9,30 @@ i번째 줄의 j번째 숫자가 1인 경우에는 i에서 j로 가는 간선이
 출력
 총 N개의 줄에 걸쳐서 문제의 정답을 인접행렬 형식으로 출력한다. 정점 i에서 j로 가는 경로가 있으면 i번째 줄의 j번째 숫자를 1로, 없으면 0으로 출력해야 한다.
 '''
+#index로 보는거임 value가 아님
 import sys
+def dfs(from_):
+    global ans_arr
+    st = []
+    for e,data in enumerate(arr[from_]):
+        if data == 1:
+            st.append((from_,e))
+
+    while st:
+        y,x = st.pop()
+        if ans_arr[y][x] >0:
+            continue
+        ans_arr[y][x] = 1
+        for i in range(N):
+            if arr[x][i] == 1 and ans_arr[x][i] == 0:
+                st.append((x,i))
+
 N = int(input())
 arr = [list(map(int, sys.stdin.readline().split()))for _ in range(N)]
 ans_arr = [[0]*N for _ in range(N)]
 
-for k in range(N):
-    for i in range(N):
-        for j in range(N):
-            ans_arr[i][j] = max(ans_arr[i][j], max(arr[i][j], arr[i][k]*arr[k][j]))
+for i in range(N):
+    dfs(i)
             
 for a in ans_arr:
     print(' '.join(map(str, a)))
