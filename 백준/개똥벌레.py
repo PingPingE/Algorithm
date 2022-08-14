6b57aa65ff67cf70d4bcdad8365e8e941aa3ce75
PingPingE <ds03023@gmail.com>
2022-08-14 20:46:39 +0900

    trying to solve a problem

--- 백준/개똥벌레.py
+++ 백준/개똥벌레.py
@@ -5,8 +5,7 @@
 아래 그림은 길이가 14미터이고 높이가 5미터인 동굴이다. (예제 그림)
 
 이 개똥벌레는 장애물을 피하지 않는다. 자신이 지나갈 구간을 정한 다음 일직선으로 지나가면서 만나는 모든 장애물을 파괴한다.
-위의 그림에서 4번째 구간으로 개똥벌레가 날아간다면 파괴해야하는 장애물의 수는 총 여덟개이다. (4번째 구간
-은 길이가 3인 석순과 길이가 4인 석순의 중간지점을 말한다)
+위의 그림에서 4번째 구간으로 개똥벌레가 날아간다면 파괴해야하는 장애물의 수는 총 여덟개이다. (4번째 구은 길이가 3인 석순과 길이가 4인 석순의 중간지점을 말한다)
 
 하지만, 첫 번째 구간이나 다섯 번째 구간으로 날아간다면 개똥벌레는 장애물 일곱개만 파괴하면 된다.
 
@@ -31,28 +30,25 @@ for n in range(1, N+1):
     else:
         info['top'].append(int(sys.stdin.readline()))
 
-bottom_N , top_N = len(info['bottom']), len(info['top'])
 bottom_counter = Counter(info['bottom'])
 top_counter = Counter(info['top'])
 
-ans = [N+1 for _ in range(H+1)]
+ans = [N+2 for _ in range(H+1)]
 for h in range(1,H+1): #종유석 >= h, 석순 >= H-h 개수 카운트
     b_count, t_count =0,0
 
-    for value in range(h, H+1):
+    for value in range(h,H+1):
         try:
             t_count += top_counter[value]
         except:
             continue
 
-    for value in range(H-h, H+1):
+    for value in range(H-h+1, H+1):
         try:
             b_count += bottom_counter[value]
         except:
             continue
-    print(b_count, t_count)
     ans[h] = b_count + t_count
 
-print(ans)
-
-print(top_counter, bottom_counter)
+min_value = min(ans)
+print(min_value, len(list(filter(lambda x: x==min_value, ans))))
