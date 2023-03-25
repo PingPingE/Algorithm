27870387b2e529c5d22bd06d8242a51d162c2b09
PingPingE <ds03023@gmail.com>
2023-03-25 19:38:32 +0900

    trying to solve a problem

--- Programmers/카카오/두큐합같게만들기.py
+++ Programmers/카카오/두큐합같게만들기.py
@@ -1,39 +1,29 @@
 from collections import deque
 def solution(queue1, queue2):
-    answer = -1
-    N = sum(queue1) + sum(queue2)
-
+    s_q1, s_q2 = sum(queue1), sum(queue2)
+    N = s_q1 + s_q2
+    cnt = 0
+    #구성이 같은지도 봐야할것 같은데
     if N % 2 == 0:
+        q1, q2 = deque(queue1), deque(queue2)
         M = N // 2
-        que = deque([[queue1, queue2, 0]])
-        done = set()
-        done.add(f"{sorted(queue1)}")
-        done.add(f"{sorted(queue2)}")
-        while que:
-            q1, q2, n = que.popleft()
-            if sum(q1) == M:
-                if answer == -1:
-                    answer = n
-                else:
-                    answer = min(n, answer)
-                continue
+        while s_q1 != M:
+            if s_q1 < M:
+                q2_pop = q2.popleft()
+                q1.append(q2_pop)
+                s_q1 += q2_pop
+                s_q2 -= q2_pop
+                cnt += 1
+            else:
+                q1_pop = q1.popleft()
+                q2.append(q1_pop)
+                s_q1 -= q1_pop
+                s_q2 += q1_pop
+                cnt += 1
 
-            try:
-                t1 = q1[1:]
-                t2 = q2[:] + [q1[0]]
-                if f"{sorted(t2)}" not in done:
-                    que.append([q1[1:], q2[:] + [q1[0]], n + 1])
-                    done.add(f"{sorted(t2)}")
-            except:
-                pass
-            try:
-                t1 = q1[:]+[q2[0]]
-                t2 = q2[1:]
-                if f"{sorted(t1)}" not in done:
-                    que.append([q1[:]+[q2[0]], q2[1:], n + 1])
-                    done.add(f"{sorted(t1)}")
-            except:
-                pass
-    return answer
+            if not q1 or not q2:
+                return -1
 
-print(solution([3, 2, 7, 2]	, [4, 6, 5, 1]))
+        return cnt
+    else:
+        return -1
