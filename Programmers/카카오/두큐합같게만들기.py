f1d99148f191aef02eb8a4f87663f814e906d1ad
PingPingE <ds03023@gmail.com>
2023-03-26 20:51:43 +0900

    solve a problem

--- Programmers/카카오/두큐합같게만들기.py
+++ Programmers/카카오/두큐합같게만들기.py
@@ -1,12 +1,17 @@
 from collections import deque
 def solution(queue1, queue2):
     s_q1, s_q2 = sum(queue1), sum(queue2)
+    max_n = max(max(queue1), max(queue2))
     N = s_q1 + s_q2
+
+    #똑같은 구성으로 계속 반복되는지 체크를 해야하는데
+    #일일이 다 기록하기에는 너무 방대한 양이라 limit을 두는 것으로 ㄱㄱ
+    cnt_limit = len(queue1) * 4
     cnt = 0
-    #구성이 같은지도 봐야할것 같은데
-    if N % 2 == 0:
+    M = N // 2
+
+    if N % 2 == 0 and max_n <= M:
         q1, q2 = deque(queue1), deque(queue2)
-        M = N // 2
         while s_q1 != M:
             if s_q1 < M:
                 q2_pop = q2.popleft()
@@ -22,6 +27,8 @@ def solution(queue1, queue2):
                 cnt += 1
 
             if not q1 or not q2:
+                return -1
+            if cnt > cnt_limit:
                 return -1
 
         return cnt
