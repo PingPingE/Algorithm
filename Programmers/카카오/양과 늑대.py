250a733a726ed2c96d57977c109f9efa3617b029
PingPingE <ds03023@gmail.com>
2023-10-15 22:00:06 +0900

    solve a problem

--- Programmers/카카오/양과 늑대.py
+++ Programmers/카카오/양과 늑대.py
@@ -1,3 +1,33 @@
+
+'''
+그냥 방문한 적 없는 자식 노드 계속 체크하고 돌아주면 됨
+그리고 돌고 나서는 또 풀어줘서 백트레킹하고...
+'''
+def solution(info, edges):
+    answer = []
+    visited = [0]*len(info)
+    def dfs(s_count, w_count):
+        if s_count > w_count:
+            answer.append(s_count)
+        else:
+            return
+        for from_node, to_node in edges:
+            if visited[from_node] and not visited[to_node]:
+                visited[to_node] = 1
+                if info[to_node] == 0:
+                    dfs(s_count+1, w_count)
+                else:
+                    dfs(s_count, w_count+1)
+                visited[to_node] = 0
+            else: pass
+    visited[0] = 1
+    dfs(1,0)
+    return max(answer)
+
+
+''''
+삽질
+'''
 from collections import deque, defaultdict
 
 def solution(info, edges):
@@ -35,19 +65,19 @@ def solution(info, edges):
 
         # 역방향 체크
         if visited and r_check(visited[-1], visited):
-            que.append([s_count, w_count, cur_node, visited])
+            que.append([s_count, w_count, cur_node, visited[:]])
 
         visited.append(cur_node)
         answer = max(s_count, answer)
         for node in links[cur_node]:
             if node not in visited:
                 if info[node] == 0:
-                    que.append([s_count + 1, w_count, node, visited])
+                    que.append([s_count + 1, w_count, node, visited[:]])
                 else:
-                    que.append([s_count, w_count + 1, node, visited])
+                    que.append([s_count, w_count + 1, node, visited[:]])
 
             # 순방향 체크
             elif check(node, visited):
-                que.append([s_count, w_count, node, visited])
+                que.append([s_count, w_count, node, visited[:]])
 
     return answer
