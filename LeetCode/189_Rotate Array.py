48d2fa302e84c8148a8675fa81db3af090903239
PingPingE <ds03023@gmail.com>
2024-07-28 21:04:06 +0900

    solve a problem

---
+++ LeetCode/189_Rotate Array.py
@@ -0,0 +1,26 @@
+'''
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+'''
+from collections import deque
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        deq = deque()
+        N = k%len(nums) 
+        while N:
+            N-=1
+            deq.appendleft(nums.pop())
+        nums[:] = list(deq) + nums[:]
